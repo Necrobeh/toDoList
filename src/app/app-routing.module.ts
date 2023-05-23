402c35de323b4218a577d383d4b5eafe630f9166
Mickaël Blanchet <mickael.blanchet5555@gmail.com>
2023-05-23 13:26:49 +0200

    get task details when clicked on in the overview list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TaskOverViewComponent } from './components/task-over-view/task-over-view.component';
 import { CreateNewTaskComponent } from './components/create-new-task/create-new-task.component';
+import { TaskDetailsComponent } from './components/task-details/task-details.component';
 
 const routes: Routes = [
   {path : "home", component : TaskOverViewComponent},
   {path : "new-task", component : CreateNewTaskComponent},
+  {path : "details/:id", component : TaskDetailsComponent},
   {path : "**", component : TaskOverViewComponent},
 ];
 
